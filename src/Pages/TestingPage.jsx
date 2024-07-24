278aa788d9b090b7f15e05d1e2468aed0c977a3a
ParthTyagi15 <95574752+ParthTyagi15@users.noreply.github.com>
2024-07-24 18:22:13 +0530

    frontend changes

---
+++ src/Pages/TestingPage.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+const TestingPage = () => {
+  return (
+    <div className="px-[100px]">
+      <div>TestingPage</div>
+      <div>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry.Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry.Lorem Ipsum is simply dummy text of the
+        printing and typesetting industry.Lorem Ipsum is simply dummy text of
+        the printing and typesetting industry.Lorem Ipsum is simply dummy text
+        of the printing and typesetting industry.Lorem Ipsum is simply dummy
+        text of the printing and typesetting industry.Lorem Ipsum is simply
+        dummy text of the printing and typesetting industry.Lorem Ipsum is
+        simply dummy text of the printing and typesetting industry.Lorem Ipsum
+        is simply dummy text of the printing and typesetting industry.Lorem
+        Ipsum is simply dummy text of the printing and typesetting
+        industry.Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry.Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry.Lorem Ipsum is simply dummy text of the
+        printing and typesetting industry.Lorem Ipsum is simply dummy text of
+        the printing and typesetting industry.Lorem Ipsum is simply dummy text
+        of the printing and typesetting industry.Lorem Ipsum is simply dummy
+        text of the printing and typesetting industry.Lorem Ipsum is simply
+        dummy text of the printing and typesetting industry.Lorem Ipsum is
+        simply dummy text of the printing and typesetting industry.Lorem Ipsum
+        is simply dummy text of the printing and typesetting industry.Lorem
+        Ipsum is simply dummy text of the printing and typesetting
+        industry.Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry.Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry.Lorem Ipsum is simply dummy text of the
+        printing and typesetting industry.Lorem Ipsum is simply dummy text of
+        the printing and typesetting industry.Lorem Ipsum is simply dummy text
+        of the printing and typesetting industry.Lorem Ipsum is simply dummy
+        text of the printing and typesetting industry.Lorem Ipsum is simply
+        dummy text of the printing and typesetting industry.Lorem Ipsum is
+        simply dummy text of the printing and typesetting industry.Lorem Ipsum
+        is simply dummy text of the printing and typesetting industry.Lorem
+        Ipsum is simply dummy text of the printing and typesetting
+        industry.Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry.Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry.Lorem Ipsum is simply dummy text of the
+        printing and typesetting industry.Lorem Ipsum is simply dummy text of
+        the printing and typesetting industry.Lorem Ipsum is simply dummy text
+        of the printing and typesetting industry.Lorem Ipsum is simply dummy
+        text of the printing and typesetting industry.Lorem Ipsum is simply
+        dummy text of the printing and typesetting industry.Lorem Ipsum is
+        simply dummy text of the printing and typesetting industry.Lorem Ipsum
+        is simply dummy text of the printing and typesetting industry.Lorem
+        Ipsum is simply dummy text of the printing and typesetting
+        industry.Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry.Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry.Lorem Ipsum is simply dummy text of the
+        printing and typesetting industry.Lorem Ipsum is simply dummy text of
+        the printing and typesetting industry.Lorem Ipsum is simply dummy text
+        of the printing and typesetting industry.Lorem Ipsum is simply dummy
+        text of the printing and typesetting industry.Lorem Ipsum is simply
+        dummy text of the printing and typesetting industry.Lorem Ipsum is
+        simply dummy text of the printing and typesetting industry.Lorem Ipsum
+        is simply dummy text of the printing and typesetting industry.Lorem
+        Ipsum is simply dummy text of the printing and typesetting
+        industry.Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry.
+      </div>
+      <h1 className="text-3xl text-red font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default TestingPage;
